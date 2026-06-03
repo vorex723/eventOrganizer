6fd8b33d721d283ed67a3e134795713ffaabcd8c
Mateusz Mazurek <mateuszmazurek@protonmail.com>
2026-06-03 14:50:46 +0200

    feat(openApi): add simple openApi variant

--- src/main/java/com/mazurek/eventOrganizer/config/SecurityConfig.java
+++ src/main/java/com/mazurek/eventOrganizer/config/SecurityConfig.java
@@ -25,6 +25,12 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/v1/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
